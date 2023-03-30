71db713681ab2b2f93cabd3ad451c54eb3bd3d69
RoshanAli339 <roshanali339lps@gmail.com>
2023-03-30 23:11:34 +0530

    Thu Mar 30 23:11:34 IST 2023

---
+++ timeScript1.js
@@ -0,0 +1,16 @@
+var cs = document.querySelectorAll(".cs");
+
+for (let i = 0; i < cs.length; i++)
+{
+    const subject = document.querySelector(".subj");
+    const professor = document.querySelector(".prof");
+    cs[i].id = i;
+    cs[i].addEventListener("mouseover", ()=>{
+        subject.textContent = "Subject: Computational Statistics";
+        professor.textContent = "Teacher: Dr. B.Srinivasa Rao"
+    })
+    cs[i].addEventListener("mouseout",()=>{
+        subject.textContent = "";
+        professor.textContent = "";
+    })
+}
