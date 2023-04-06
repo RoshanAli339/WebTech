99c81458641671916a0668b3bfe9948211192065
RoshanAli339 <roshanali339lps@gmail.com>
2023-04-06 22:12:19 +0530

    Thursday 06 April 2023 10:12:19 PM IST

--- timeScript1.js
+++ timeScript1.js
@@ -5,12 +5,8 @@ for (let i = 0; i < cs.length; i++)
     const subject = document.querySelector(".subj");
     const professor = document.querySelector(".prof");
     cs[i].id = i;
-    cs[i].addEventListener("mouseover", ()=>{
+    cs[i].addEventListener("onclick", ()=>{
         subject.textContent = "Subject: Computational Statistics";
         professor.textContent = "Teacher: Dr. B.Srinivasa Rao"
-    })
-    cs[i].addEventListener("mouseout",()=>{
-        subject.textContent = "";
-        professor.textContent = "";
     })
 }
